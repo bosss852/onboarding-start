d19ff5ec558faf87bf8cc6a8fa19491707fe73a2
bosss852 <oreoluwaolwadare@gmail.com>
2025-10-28 22:47:23 -0400

    Update spi_peri.v

--- src/spi_peri.v
+++ src/spi_peri.v
@@ -4,11 +4,11 @@ module spi_peri (
     input  wire       copi,      // clock
     input  wire       nCS,     // reset_n - low to reset
     input  wire       SCLK,
-    input  wire [7:0] en_reg_out_7_0,
-    input  wire [7:0] en_reg_out_15_8,
-    input  wire [7:0] en_reg_pwm_7_0,
-    input  wire [7:0] en_reg_pwm_15_8,
-    input  wire [7:0] pwm_duty_cycle,
+    output  reg [7:0] en_reg_out_7_0,
+    output  reg [7:0] en_reg_out_15_8,
+    output  reg [7:0] en_reg_pwm_7_0,
+    output  reg [7:0] en_reg_pwm_15_8,
+    output  reg [7:0] pwm_duty_cycle,
 );
 
 endmodule
