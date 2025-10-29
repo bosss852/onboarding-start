e99011814f0a7f6e2113a63457b6a9e45fe49957
bosss852 <oreoluwaolwadare@gmail.com>
2025-10-28 20:38:03 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,8 +17,6 @@ module tt_um_onboarding_Ore (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
   assign uio_oe = 8'hFF;
 
   wire [7:0] en_reg_out_7_0;
@@ -38,7 +36,6 @@ module tt_um_onboarding_Ore (
     .out({uio_out, uo_out})
   );
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
