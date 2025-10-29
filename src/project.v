fa2f44e9b50b9723f363da8f0da34178bd8179b2
bosss852 <oreoluwaolwadare@gmail.com>
2025-10-28 21:04:05 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -33,7 +33,7 @@ module tt_um_onboarding_Ore (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwn_duty_cycle)
+    .pwm_duty_cycle(pwm_duty_cycle)
   );
 
   pwm_peripheral pwm_peripheral_inst (
